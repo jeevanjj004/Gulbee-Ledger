99f8bda200d736e4f80acc94716dbe6e53546b78
Thomas Jacob <jeevanjj004@gmail.com>
2025-12-22 13:18:38 +0530

    Set root URL to redirect to /user/

--- exp_tracker/urls.py
+++ exp_tracker/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('income/', include('income.urls')),
@@ -10,12 +11,11 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('expense/', include('expense.urls')),
     path('emi/', include('emi.urls')),
-        path('reports/', include('reports.urls')),
+    path('reports/', include('reports.urls')),
 
-
-
-
-
+    # Redirect root URL to /user/
+    path('', lambda request: redirect('user/', permanent=False)),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
