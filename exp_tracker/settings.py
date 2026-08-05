170ba1822512e77c6a0a6a8d2432eef74cc8f443
jeevanjj004 <jeevanjj004@gmail.com>
2026-08-05 21:13:47 +0530

    Replace Gmail SMTP with Brevo SMTP for production email delivery

--- exp_tracker/settings.py
+++ exp_tracker/settings.py
@@ -140,19 +140,23 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL = "login"
 LOGOUT_REDIRECT_URL = "login"
 
+
+
 # =========================
 # EMAIL CONFIG
 # =========================
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
 
 
 #cron secret id
