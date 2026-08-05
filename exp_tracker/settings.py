6eb984d1370e6cb602f6bd3e97eb8ded2a665611
jeevanjj004 <jeevanjj004@gmail.com>
2026-08-05 21:50:13 +0530

    .env updated

--- exp_tracker/settings.py
+++ exp_tracker/settings.py
@@ -145,19 +145,20 @@ LOGOUT_REDIRECT_URL = "login"
 # =========================
 # EMAIL CONFIG
 # =========================
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 30
 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 #cron secret id
 
