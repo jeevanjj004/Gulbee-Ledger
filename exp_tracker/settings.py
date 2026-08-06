acaf8a07853d1d4f7673ff78a18fd5c4ef77974b
jeevanjj004 <jeevanjj004@gmail.com>
2026-08-06 11:00:53 +0530

    - Replaced SMTP email sending with Brevo Transactional Email HTTP API. - Added reusable Brevo email utility (exp_tracker/utils/brevo_mail.py). - Removed dependency on SMTP for email delivery. - Fixed email delivery on Render Free plan by avoiding blocked SMTP ports. - Updated EMI reminder management command to use Brevo API. - Added success/error logging with Brevo message IDs. - Mark reminder flags only after successful email delivery. - Added BREVO_API_KEY environment variable support. - Simplified email configuration by removing SMTP dependency. - Verified automated reminder workflow through Render Cron. - Successfully tested:   * 5-day EMI reminders   * Due-day EMI reminders   * Reminder flag updates   * Render production deployment

--- exp_tracker/settings.py
+++ exp_tracker/settings.py
@@ -142,21 +142,6 @@ LOGOUT_REDIRECT_URL = "login"
 
 
 
-# =========================
-# EMAIL CONFIG
-# =========================
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# EMAIL_HOST = os.getenv("EMAIL_HOST")
-# EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-
-# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-# EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
-
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-# EMAIL_TIMEOUT = 60
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
