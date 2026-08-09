103f8e3f0bd037bd16deb77cabad012a1d6df900
jeevanjj004 <jeevanjj004@gmail.com>
2026-08-09 13:37:04 +0530

    Fix GulbeeLedger PWA service worker

--- templates/sw.js
+++ templates/sw.js
@@ -1,65 +1,86 @@
-const CACHE_NAME="gulbeeledger-v2";
+const CACHE_NAME = "gulbeeledger-v3";
+
+const OFFLINE_URL = "/user/";
+
 const FILES_TO_CACHE = [
     "/",
     "/user/",
     "/static/manifest.json",
+    "/static/icons/icon-192.png",
+    "/static/icons/icon-512.png",
+    "/static/icons/maskable-192.png",
+    "/static/icons/maskable-512.png",
+    "/static/icons/favicon-32.png"
 ];
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME)
-                        return caches.delete(key);
-                })
-            )
-        )
+        caches.keys()
+            .then(keys => {
+                return Promise.all(
+                    keys
+                        .filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
+                );
+            })
+            .then(() => self.clients.claim())
     );
-    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
 
-    if (event.request.method !== "GET")
+    if (event.request.method !== "GET") {
         return;
+    }
 
     event.respondWith(
-
         fetch(event.request)
             .then(response => {
 
-                let copy = response.clone();
+                // Cache successful responses
+                if (response && response.status === 200) {
+                    const responseClone = response.clone();
 
-                caches.open(CACHE_NAME)
-                    .then(cache => cache.put(event.request, copy));
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(event.request, responseClone);
+                        });
+                }
 
                 return response;
-
             })
-
             .catch(() => {
 
                 return caches.match(event.request)
-                    .then(res => {
+                    .then(cachedResponse => {
 
-                        if (res)
-                            return res;
+                        if (cachedResponse) {
+                            return cachedResponse;
+                        }
 
-                        return caches.match("/user/");
+                        // For navigation requests, show cached app page
+                        if (event.request.mode === "navigate") {
+                            return caches.match(OFFLINE_URL);
+                        }
 
+                        return new Response(
+                            "You are offline.",
+                            {
+                                status: 503,
+                                headers: {
+                                    "Content-Type": "text/plain"
+                                }
+                            }
+                        );
                     });
-
             })
-
     );
-
 });
